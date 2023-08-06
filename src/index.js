a0cb014ce2e055b935141326ce750ac422464922
fithaashraf17 <fithaashraf172@gmail.com>
2023-08-06 21:00:43 +0530

    handling hoverevent and improved reusability with props

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+function Test() {
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+
+    <StarRating size={24} color="red" defaultRating={3} />
   </React.StrictMode>
 );
