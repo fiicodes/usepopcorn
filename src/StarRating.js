0de7e6b63e3c8caeb402cdaa42b469261b9c5cf2
fithaashraf17 <fithaashraf172@gmail.com>
2023-08-16 21:34:40 +0530

    fetching

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
+
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -8,12 +10,21 @@ const containerStyle = {
 const starContainerStyle = {
   display: "flex",
 };
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  onSetRating: PropTypes.func,
+};
 
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   messages = [],
   defaultRating = 0,
+  onSetRating,
 
   size = 48,
 }) {
@@ -21,6 +32,7 @@ export default function StarRating({
   const [tempRating, setTemprating] = useState(0);
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
   const textStyle = {
     lineHeight: "1",
