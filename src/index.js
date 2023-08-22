9033d84391cd808acec723548df4e2e1a3c78260
fithaashraf17 <fithaashraf172@gmail.com>
2023-08-22 13:39:49 +0530

    added cleanup functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App copy.js";
+import App from "./App.js";
 // import StarRating from "./StarRating";
 // function Test() {
 //   const [movieRating, setMovieRating] = useState(0);
